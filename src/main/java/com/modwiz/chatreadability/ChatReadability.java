c9f09a75235efcd2d982a6702ad652e5e353a0e4
Iris Johnson <iris@modwiz.com>
2020-10-23 00:00:19 -0500

    Fix issue with loading on 1.8 Java runtimes

--- src/main/java/com/modwiz/chatreadability/ChatReadability.java
+++ src/main/java/com/modwiz/chatreadability/ChatReadability.java
@@ -1,5 +1,6 @@
 package com.modwiz.chatreadability;
 
+import com.google.common.collect.ImmutableList;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
@@ -7,10 +8,8 @@ import org.bukkit.plugin.java.annotation.plugin.Description;
 import org.bukkit.plugin.java.annotation.plugin.Plugin;
 import org.bukkit.plugin.java.annotation.plugin.author.Author;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
-import java.util.stream.Collectors;
 
 @Plugin(name="ChatReadability", version="0.0.1")
 @Description("Gives users randomized name colors and alternates chat colors")
@@ -19,9 +18,9 @@ import java.util.stream.Collectors;
 public class ChatReadability extends JavaPlugin {
 
     // Not configurable for now, uses bungee api colors since I think that's what we need to use TextComponent easily
-    private List<ChatColor> messageColorList = List.of(ChatColor.GRAY);
+    private List<ChatColor> messageColorList = ImmutableList.of(ChatColor.GRAY);
     // Always have a default value in case it's not initialized properly (since we don't check for that)
-    private List<ChatColor> nameColorList = List.of(ChatColor.DARK_AQUA);
+    private List<ChatColor> nameColorList = ImmutableList.of(ChatColor.DARK_AQUA);
     //
 
     @Override
@@ -37,14 +36,14 @@ public class ChatReadability extends JavaPlugin {
                 // I think this seems nice and clean? Possibly a waste.
                 try {
                     nameColorList = nameColorStrings.parallelStream().map(ChatColor::of)
-                            .collect(Collectors.toUnmodifiableList());
+                            .collect(ImmutableList.toImmutableList());
                 } catch (IllegalArgumentException e) {
                     getLogger().log(Level.SEVERE, "Encountered an invalid color value in the name colors", e);
                 }
             }
             // Handle "enabling" alternating message colors by adding the second color
             if (getConfig().getBoolean("alternating-colors")) {
-                messageColorList = List.of(ChatColor.GRAY, ChatColor.DARK_GRAY);
+                messageColorList = ImmutableList.of(ChatColor.GRAY, ChatColor.DARK_GRAY);
             }
             getServer().getPluginManager().registerEvents(new ChatListener(this), this);
         }
